92a0879adb56d7cca39a087f2bd95f66aac95b09
Francesc Xavier Bosch Janer <xavi2794@gmail.com>
2023-01-20 23:56:19 +0100

    ECMAScript10

---
+++ src/es10/01-trimstart-trimend.js
@@ -0,0 +1,3 @@
+const hello = '       Helo, World!     ';
+console.log(hello.trimStart());
+console.log(hello.trimEnd());
---
+++ src/es10/02-try-catch.js
@@ -0,0 +1,11 @@
+try {
+    hello();
+} catch {
+    console.log(error);
+}
+
+try {
+    anotherFn();
+} catch {
+    console.log('Esto es un error');
+}
